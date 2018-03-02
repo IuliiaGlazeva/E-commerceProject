8b40ea190dbdbfc49c1491cc7d169b6156e511c6
¨iuliia¨ <yuliya.glazeva@gmail.com>
2018-03-02 11:39:16 +0100

    added port 4001

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express')
+const Sequelize = require('sequelize')
 const bodyParser = require('body-parser')
-const cors = require('cors')
-const db = require('./models')
+
 const app = express()
 
-var Sequelize = require('sequelize')
-var sequelize = new Sequelize('postgres://postgres:iuliia@localhost:5432/postgres')
+var sequelize = new Sequelize('postgres://postgres:secret@localhost:5432/postgres')
 const port = process.env.PORT || 4001
 
 const Product = sequelize.define('product', {
@@ -27,39 +26,98 @@ const Product = sequelize.define('product', {
   timestamps: false
 })
 
-
-//var Product = db.define('Product', {
-////  name: Sequelize.STRING,
-  //score: Sequelize.INTEGER
-//}, {
-  //tableName: 'Products'
-//})
-
-
-
-app.get('/products', (request, response) => {
-  Product.findAll().then(products => {
-    response.send({ products })
-  })
+app.listen(port, 4001, () => {
+  console.log('Express API listening on port 4001')
 })
 
-app.get('/products/:id', (request, response) => {
-  const productId = request.params.id
-  Product.findById(productId).then(product => {
-    response.send(product)
-  })
-})
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Yo!' })
-})
-
-app.listen(4001, () => console.log('Express API listening on port 4001'))
-
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')
   next()
 })
-Product.findById(1).then(product => console.log(JSON.stringify(product)))
+
+app.use(bodyParser.json())
+
+app.get('/products', (req, res) => {
+  Product.findAll({
+    attributes: ['id', 'name', 'price']
+  })
+    .then(result => {
+      res.json(result)
+    })
+    .catch(err => {
+      res.status(500)
+      res.json({message: 'Something went wrong'})
+    })
+})
+
+app.get('/products/:id', (req, res) => {
+  Product.findById(req.params.id)
+    .then(result => {
+      if (result) {
+        res.json(result)
+      } else {
+        res.status(404)
+        res.json({ message: 'Not Found' })
+      }
+    })
+    .catch(err => {
+      res.status(500)
+      res.json({ message: 'There was an error' })
+    })
+})
+
+app.post('/products', (req, res) => {
+  const product = req.body
+
+  Product.create(product)
+    .then(entity => {
+      res.status(201)
+      res.json(entity)
+    })
+    .catch(err => {
+      res.status(422)
+      res.json({ message: err.message })
+    })
+})
+
+const updateOrPatch = (req, res) => {
+  const productId = Number(req.params.id)
+  const updates = req.body
+
+  Product.findById(req.params.id)
+    .then(entity => {
+      return entity.update(updates)
+    })
+    .then(final => {
+      res.json(final)
+    })
+    .catch(error => {
+      res.status(500).send({
+        message: `Something went wrong`,
+        error
+      })
+    })
+}
+
+app.put('/products/:id', updateOrPatch)
+app.patch('/products/:id', updateOrPatch)
+
+app.delete('/products/:id', (req, res) => {
+  Product.findById(req.params.id)
+    .then(entity => {
+      return entity.destroy()
+    })
+    .then(_ => {
+      res.send({
+        message: 'The product was deleted succesfully'
+      })
+    })
+    .catch(error => {
+      res.status(500).send({
+        message: `Something went wrong`,
+        error
+      })
+    })
+})
